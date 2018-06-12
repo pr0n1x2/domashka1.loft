65be33ddcf8c439f6af2c6c13c4a1e7bfffaeec1
Vladimir Skirga <vladimir.skirga84@gmail.com>
2018-06-12 13:09:19 +0300

    Второе задание

---
+++ task2.php
@@ -0,0 +1,9 @@
+<?php
+
+const TOTAL_IMAGES = 80;
+const FLOMASTERS = 23;
+const KARANDASHI = 40;
+
+$kraskami = TOTAL_IMAGES - FLOMASTERS - KARANDASHI;
+
+echo "Красками выполнено $kraskami рисунков!";
